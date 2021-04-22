cea829fa587c7029d76d953d8534a826fb615672
snowie2000 <mactype.feedback@gmail.com>
2021-04-22 11:30:53 +0800

    added serviceworker and PWA support

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import r from "@/config/routes"
 
+(function () {
+  if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js');
+  }
+})()
+
 Vue.config.productionTip = false
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,36 @@
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 //     .BundleAnalyzerPlugin;
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require("path");
 
 module.exports = {
     productionSourceMap: process.env.NODE_ENV !== "production",
     configureWebpack: {
         plugins: [
             // new BundleAnalyzerPlugin()
+            new SWPrecacheWebpackPlugin({
+                cacheId: 'crypto-lib-v1',
+                filename: 'sw.js',
+                minify: true,
+                mergeStaticsConfig: true, // if you don't set this to true, you won't see any webpack-emitted assets in your serviceworker config
+                staticFileGlobsIgnorePatterns: [/\.map$/], // use this to ignore sourcemap files
+            }),
+            new WebpackPwaManifest({
+                name: 'Crypto WorkShop',
+                short_name: 'CryptoWS',
+                description: 'A Handy crypto lab on your palm!',
+                background_color: '#ffffff',
+                ios: true,
+                icons: [
+                    {
+                        src: path.resolve('public/favicon.png'),
+                        sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+                        ios: true,
+                        purpose: 'maskable'
+                    }
+                ]
+            })
         ]
     }
 }
