07c04da688a65a1f823708d71e922903d7ed29ac
snowie2000 <mactype.feedback@gmail.com>
2021-04-23 15:11:35 +0800

    able to scroll the tab with touchscreen

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <el-tabs :value="activeName" @tab-click="switchTable">
+    <el-tabs :value="activeName" @tab-click="switchTable" ref="tab">
       <el-tab-pane v-for="label in ['base64', 'md5', 'json', 'hmac', 'regex']" :key="label" :label="label.toUpperCase()" :name="label"></el-tab-pane>
     </el-tabs>
     <keep-alive>
@@ -17,7 +17,13 @@ export default {
   name: 'App',
   data: function () {
     return {
-      activeName: "base64"
+      activeName: "base64",
+      tabDrag: {
+        touchX: 0,
+        offsetStart: 0,
+        tablist: null,
+        maxShift: 0,
+      }
     }
   },
   methods: {
@@ -27,6 +33,27 @@ export default {
           name: tab.name
         })
       }
+    },
+    touchstart(e) {
+      this.tabDrag.touchX = e.touches[0].clientX
+      this.tabDrag.tablist = this.$refs.tab.$el.querySelector("div[role=tablist]")
+      this.tabDrag.maxShift = this.tabDrag.tablist.clientWidth - this.tabDrag.tablist.parentElement.clientWidth
+      this.tabDrag.offsetStart = this.tabDrag.tablist.parentElement.getBoundingClientRect().x - this.tabDrag.tablist.getBoundingClientRect().x
+    },
+    touchmove(e) {
+      let delta = this.tabDrag.touchX - e.touches[0].clientX
+      if (delta > 10 || delta< -10) {
+        this.tabDrag.tablist.classList.add("dragging")
+        // this.tabDrag.tablist.style["transform"] = "translateX(" + (this.tabDrag.offsetStart + delta) + "px)"
+        let offset = this.tabDrag.offsetStart + delta
+        offset = offset<0?0:(offset>this.maxShift?this.maxShift:offset)
+        this.$refs.tab.$children[0].$data.navOffset = offset
+      }
+    },
+    touchend() {
+      if (this.tabDrag.tablist.classList.contains("dragging")) {
+        this.tabDrag.tablist.classList.remove("dragging")
+      }
     }
   },
   created() {
@@ -35,7 +62,12 @@ export default {
     })
   },
   mounted() {
-    this.activeName = this.$route.name
+    this.activeName = this.$route.name;
+    ["touchstart", "touchmove", "touchend"].forEach(v=> {
+      this.$refs.tab.$el.addEventListener(v, (e) => {
+        this[v](e)
+      })
+    })
   }
 }
 </script>
@@ -73,6 +105,10 @@ textarea.ele {
     outline: 0;
     border-color: #409EFF;
   }
+}
+
+.el-tabs__nav.dragging {
+  transition: none;
 }
 
 .text-centered {
